677210156e16ac417985b9f18410232b04fb7b0e
DVSB <vzcrsa12@googlemail.com>
2019-07-09 16:44:38 +0200

    Update de-de.js

--- BTPanel/static/language/de-de.js
+++ BTPanel/static/language/de-de.js
@@ -1,1 +1,19 @@
-
+var BT_MSG = {
+	"ERROR":"Operation fehlgeschlagen",
+	"SUCCESS":"Erfolgreiche Operation",
+	"ADD_SUCCESS":"Erfolgreich hinzugefügt",
+	"ADD_ERROR":"Hinzufügen Error",
+	"DEL_SUCCESS":"Erfolgreich gelöscht",
+	"DEL_ERROR":"Löschen Error",
+	"SET_SUCCESS":"Erfolgreiches Setup",
+	"SET_ERROR":"Setup Error",
+	"PATH_ERROR":"Das Systemschlüsselverzeichnis kann nicht als Standortverzeichnis verwendet werden",
+	"SITE_ADD_SUCCESS":"Seite wurde erfolgreich erstellt!",
+	"SITE_ADD_EXISTS":"Die Seite, die Sie hinzugefügt haben, ist bereits vorhanden!",
+	"SITE_ADD_PORT":"Portbereich ist ungültig.",
+	"SITE_DEL_SUCCESS":"Seite erfolgreich gelöscht!",
+	"SITE_ADD_DOMAIN":"Domainname erfolgreich hinzugefügt!",
+	"SITE_ADD_EXISTS":"Angegebene Domainname existiert bereits!",
+	"SITE_ADD_EXISTS":"Angegebene Domainname existiert bereits!",
+	"SITE_ADD_BINDING":"Domänenname und Unterverzeichnisname dürfen nicht leer sein!"
+}
